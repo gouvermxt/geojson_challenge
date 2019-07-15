baf57022d622dbc2daf1dd81f17bd05da6c5f18e
Marcio Gouvea Silva <gouvermxt@gmail.com>
2019-07-15 18:41:23 -0300

    Add general error handling

---
+++ app/controllers/concerns/api_exception_handler.rb
@@ -0,0 +1,14 @@
+# Handles unhandled exceptions with a standard response
+module ApiExceptionHandler
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from StandardError, with: :bad_request
+  end
+
+  def bad_request
+    render json: { 
+      error: 'An unknow error happened. Please contact the system administrators.' 
+    }, status: 500
+  end
+end
